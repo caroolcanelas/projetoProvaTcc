22431d7babe75d0db84c5dc2961d44b55c8917f5
Leticia Nunes <leticianunesq8@gmail.com>
2025-04-19 19:32:04 -0300

    adiciona codigo teste do banco no application

--- src/main/java/com/projetoProvaTcc/PrjProvaApplication.java
+++ src/main/java/com/projetoProvaTcc/PrjProvaApplication.java
@@ -8,5 +8,30 @@ public class PrjProvaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrjProvaApplication.class, args);
 
+//		try {
+//			Disciplina d = new Disciplina("PROG II","Programacao de Computadores II",3, "Aprender principíos de Programacao II");
+//			DaoDisciplina daoDisciplina = new DaoDisciplina();
+//			daoDisciplina.salvar(d);
+//
+//		  Topico t = new Topico(1, "Arvore", "Explicação sobre arvore", d);
+//		  DaoTopico dao = new DaoTopico();
+//		  dao.salvar(t);
+//
+//		  Opcao o = new Opcao("A opção é a seguinte lalala", true);
+//		  DaoOpcao dao = new DaoOpcao();
+//		  dao.salvar(o);
+//
+//		  Questao q = new Questao("Instrução inicial", "Suporte", "Comando",
+//			  NivelQuestao.DIFICIL, TipoQuestao.RESPOSTA_UNICA, true);
+//		  DaoQuestao dao = new DaoQuestao();
+//		  dao.salvar(q);
+//
+//		  Tag t = new Tag("Teste", "Assunto do teste");
+//		  DaoTag dao = new DaoTag();
+//		  dao.salvar(t);
+//
+//		} catch (ModelException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
