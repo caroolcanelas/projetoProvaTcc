b6d4d37cd6a8676f29d926aaff19d44f36797b5b
Leticia Nunes <leticianunesq8@gmail.com>
2025-07-05 18:23:44 -0300

    remove codigo

--- backend/src/main/java/com/projetoProvaTcc/controller/RecursoController.java
+++ backend/src/main/java/com/projetoProvaTcc/controller/RecursoController.java
@@ -89,23 +89,4 @@ public class RecursoController {
         }
     }
 
-
-    //precisa desse método?
-    //Todos os arquivos contidos no CSV, devem existir no ZIP
-//    @Operation(summary = "Importa recursos em lote via CSV e ZIP")
-//    @PostMapping(value = "/importar-lote", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    public ResponseEntity<String> importarRecursosEmLote(
-//            @RequestParam("csv") MultipartFile csvFile,
-//            @RequestParam("zip") MultipartFile zipFile
-//    ) {
-//        try {
-//            recursoService.importarRecursosViaCsv(csvFile, zipFile);
-//            return ResponseEntity.ok("Recursos importados com sucesso!");
-//        } catch (ModelException e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        } catch (Exception e) {
-//            return ResponseEntity.internalServerError().body("Erro durante a importação: " + e.getMessage());
-//        }
-//    }
-
 }
